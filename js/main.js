f561b544b55c76495fed073035383cc113447fe2
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-30 00:10:26 +0200

    html-css-done
    rest-todoolean

--- js/main.js
+++ js/main.js
@@ -1,22 +1,43 @@
 $(document).ready(function() {
-  //invoke the inputsData function
-  inputsData();
+  //invoke the inputsData and the printData functions
+  GetData();
   printData();
 });
+
+  //make a keypress function for the input
+  $(".text_input").keypress(function(event) {
+    if (event.which == 13) {
+      printData();
+      $(".text_input").val("");
+    }
+  });
+
+  //make a click function for the button
+  $(".insert_value").click(function(){
+     printData();
+     $(".text_input").val("");
+  });
 
   //make a function printing the Data
   function printData(data) {
     //make variables for handlebars
     var source = $("#list_template").html();
     var template = Handlebars.compile(source)
-    var context = {
-      "data": data
-    };
+
+    for (var i = 0; i < data.length; i++) {
+      //prepare the context to insert it inside the handlebars Template
+      var context = {
+        "id": data[i].id,
+        "text": data[i].text
+    }
+    //make a variable html with the context inside of it
     var html = template(context);
+    // append inside the DOM
     $("#todo_list").append(html);
+   }
   }
   //make a function asking for the inputsData
-  function inputsData () {
+  function GetData () {
     //make an ajax call (read - GET)
     $.ajax(
    {
@@ -24,10 +45,19 @@ $(document).ready(function() {
      "method":"GET",
      "success":function (data) {
        printData(data)
-       console.log(data);
      },
      "error":function (error) {
-       alert("E avvenuto un errore. "+ error);
+       alert(error);
      }
    });
   }
+  //  //make a function asking to deleteData
+  //  function DeleteData(type) {
+  //   //make an ajax call (delete - DELETE)
+  //   $.ajax(
+  //   {
+  //    "url": "http://157.230.17.132:3008/todos",
+  //    "method": "DELETE",
+  //     }
+  //  });
+  // }
